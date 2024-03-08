dd6dff7e41aa820a570c1c62d9f3fe4a67f2503c
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-03-08 08:49:48 -0300

    Create submitUser function to send sign in form

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,8 +1,26 @@
+"use client";
+
+import { signInUser } from "@/services/user/signIn";
 import Link from "next/link";
+import { FormEvent, useState } from "react";
 import { FaGoogle as GoogleIcon } from "react-icons/fa";
 import { SiGithub as GithubIcon } from "react-icons/si";
 
 export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitUser = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const user = {
+      email,
+      password,
+    };
+
+    await signInUser(user);
+  };
+
   return (
     <section className="flex flex-col gap-6">
       <h1 className="text-4xl font-bold text-center mt-8">Sign in</h1>
@@ -17,12 +35,25 @@ export default function SignIn() {
         </button>
       </div>
 
-      <form className="flex flex-col gap-4 w-[70%] mx-auto">
-        <input type="email" placeholder="Email" className="p-2 bg-gray-200" />
+      <form
+        className="flex flex-col gap-4 w-[70%] mx-auto"
+        onSubmit={submitUser}
+      >
+        <input
+          type="email"
+          placeholder="Email"
+          onInput={(e) => setEmail(e.currentTarget.value)}
+          value={email}
+          className="p-2 bg-gray-200"
+          required
+        />
         <input
           type="password"
           placeholder="Password"
+          onInput={(e) => setPassword(e.currentTarget.value)}
+          value={password}
           className="p-2 bg-gray-200"
+          required
         />
 
         <button
