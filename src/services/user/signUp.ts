d5d5979fe3d040c70e1812eb63509c6ceeb16792
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-03-11 09:45:59 -0300

    Create fetch function signUpUser

---
+++ src/services/user/signUp.ts
@@ -0,0 +1,20 @@
+interface SignUpUser {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export const signUpUser = async (user: SignUpUser) => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/user/sign-up`,
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    }
+  );
+  const data = await response.json();
+  return data;
+};
