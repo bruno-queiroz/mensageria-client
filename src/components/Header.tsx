547bbcff705c988139c7bcf6edd6dcdd5181cfec
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-03-12 07:47:40 -0300

    Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="p-4">
+      <div className="flex gap-12 items-center">
+        <Link href="/" className="font-bold text-3xl">
+          Mensageria
+        </Link>
+
+        <nav>
+          <ul className="flex gap-4 items-center">
+            <li>
+              <Link href="/find-friend">Find friend</Link>
+            </li>
+            <li>
+              <Link href="/chat">Chat</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
