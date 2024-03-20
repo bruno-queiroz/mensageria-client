645fb8adf3cfcc3dffdfe58c85a9e3dfb4dc6e63
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-03-20 08:37:55 -0300

    Create Spinner component

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,7 @@
+export const Spinner = ({ color = "neutral-900" }) => {
+  return (
+    <div
+      className={`w-[24px] h-[24px] border-[5px] border-${color} mx-auto border-t-[5px] border-t-transparent rounded-full animate-spin`}
+    />
+  );
+};
