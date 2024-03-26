80fc47bda94e50c39ba2b4db1d49751de0795ca6
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-03-26 07:49:22 -0300

    Create useFindFriend custom hook

---
+++ src/hooks/useFindFriend.ts
@@ -0,0 +1,29 @@
+import { findFriend } from "@/services/user/findFriend";
+import { useMutation } from "@tanstack/react-query";
+import { FormEvent, useState } from "react";
+
+export const useFindFriend = () => {
+  const [query, setQuery] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { mutateAsync, data: users } = useMutation({
+    mutationFn: (query: string) => findFriend(query),
+  });
+
+  const handleFindFriend = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (query === "") return;
+
+    setIsLoading(true);
+    await mutateAsync(query);
+    setIsLoading(false);
+  };
+
+  return {
+    query,
+    setQuery,
+    handleFindFriend,
+    isLoading,
+    users,
+  };
+};
