009351f9e346152222cfc4f00e82beadb26ddb6e
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-03-26 07:50:30 -0300

    Add useFindFriend custom hook

--- src/app/find-friend/page.tsx
+++ src/app/find-friend/page.tsx
@@ -1,16 +1,23 @@
+"use client";
 import { FindFriendItem } from "@/components/FindFriendItem";
+import { useFindFriend } from "@/hooks/useFindFriend";
 
 export default function FindFriend() {
+  const { query, setQuery, handleFindFriend, isLoading, users } =
+    useFindFriend();
+
   return (
     <section>
       <div className="flex flex-col gap-8 items-center w-[70%] mx-auto">
         <h1 className="text-4xl font-bold text-center mt-4">Find friend</h1>
 
-        <form className="flex w-full">
+        <form className="flex w-full" onSubmit={handleFindFriend}>
           <input
             type="text"
             placeholder="Search for user id or username"
             className="bg-gray-200 p-2 w-full"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
           />
           <button className="bg-blue-200 p-2">Find</button>
         </form>
