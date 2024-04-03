16dd71cab8a70f7d7cf946d3b49d7a5336260a56
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-03 11:16:35 -0300

    Add "mensageria-user-id" cookie when signing in

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,25 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
-
+import { cookies } from "next/headers";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { Adapter } from "next-auth/adapters";
 import { connectDb } from "@/db/connect";
+import { setSessionExpireDate } from "@/utils/setSessionExpireDate";
 
 export const authOptions: NextAuthOptions = {
   adapter: DrizzleAdapter(connectDb()) as Adapter,
+  debug: true,
+  callbacks: {
+    async signIn({ user }) {
+      cookies().set("mensageria-user-id", user.id, {
+        httpOnly: false,
+        secure: true,
+        expires: setSessionExpireDate({ days: 30 }),
+      });
+      return true;
+    },
+  },
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
