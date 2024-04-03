dd775ee5f01f0fbd02afca865476708afbaf6f57
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-03 07:37:00 -0300

    Create sendFriendshipRequest fetch function

---
+++ src/services/friendship-request/sendFriendshipRequest.ts
@@ -0,0 +1,23 @@
+import { ServerResponse, User } from "../types";
+
+interface SendFriendshipRequest {
+  fromUser: string;
+  toUser: string;
+}
+
+export const sendFriendshipRequest = async (request: SendFriendshipRequest) => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/friendship-request`,
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(request),
+      credentials: "include",
+    }
+  );
+  const data: ServerResponse<User[]> = await response.json();
+
+  return data;
+};
