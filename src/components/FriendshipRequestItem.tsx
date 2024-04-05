eff29f2b1ed9b048ade73736fa1b7d65422ad8a4
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-05 08:59:29 -0300

    Add useAcceptFriendshipRequest custom hook

--- src/components/FriendshipRequestItem.tsx
+++ src/components/FriendshipRequestItem.tsx
@@ -1,6 +1,9 @@
+"use client";
 import { GetFriendshipRequest } from "@/services/friendship-request/getFriendshipRequest";
 import { FaCheck as AcceptIcon } from "react-icons/fa6";
 import { IoMdClose as RejectIcon } from "react-icons/io";
+import { Spinner } from "./Spinner";
+import { useAcceptFriendshipRequest } from "@/hooks/useAcceptFriendshipRequest";
 
 export const FriendshipRequestItem = ({
   fromUser,
@@ -8,6 +11,11 @@ export const FriendshipRequestItem = ({
   name,
   sentAt,
 }: GetFriendshipRequest) => {
+  const {
+    handleAcceptFriendshipRequest,
+    isPending: acceptIsPending,
+    isError,
+  } = useAcceptFriendshipRequest();
   return (
     <div className="flex relative w-full gap-4 items-center bg-blue-100 p-3 rounded-lg">
       <div className="w-[70px] h-[70px]">
@@ -23,11 +31,17 @@ export const FriendshipRequestItem = ({
         <span className="right-1 bottom-1 text-sm text-gray-600">aaaaa</span>
       </div>
       <div className="flex gap-2 ml-auto">
-        <button className="ml-auto bg-blue-200 p-2 text-green-500 rounded">
-          <AcceptIcon />
+        <button
+          onClick={() => handleAcceptFriendshipRequest(fromUser)}
+          className="ml-auto bg-blue-200 p-2 text-green-500 rounded"
+        >
+          {acceptIsPending ? <Spinner wh={16} /> : <AcceptIcon />}
         </button>
-        <button className="ml-auto bg-blue-200 p-2 text-red-500 rounded">
-          <RejectIcon />
+        <button
+          disabled={acceptIsPending}
+          className="ml-auto bg-blue-200 p-2 text-red-500 rounded"
+        >
+          {false ? <Spinner wh={16} /> : <RejectIcon />}
         </button>
       </div>
     </div>
