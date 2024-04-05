216969ee0f6c78c2fc2580a19dd563c08d5d9e5f
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-05 08:03:47 -0300

    Create getFriendshipRequest fetch function

---
+++ src/services/friendship-request/getFriendshipRequest.ts
@@ -0,0 +1,25 @@
+import { ServerResponse } from "../types";
+
+export interface GetFriendshipRequest {
+  name: string;
+  image: string;
+  fromUser: string;
+  sentAt: Date;
+}
+
+export const getFriendshipRequest = async (authCookie: string | undefined) => {
+  if (!authCookie) return null;
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/friendship-request`,
+    {
+      credentials: "include",
+      headers: {
+        Cookie: authCookie,
+        Origin: process.env.NEXT_PUBLIC_DEPLOY_URL!,
+      },
+    }
+  );
+  const data: ServerResponse<GetFriendshipRequest[]> = await response.json();
+  return data;
+};
