c445bb16a84c1890756f023470b379d9eefb5b9c
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-08 08:53:30 -0300

    Add wasClicked state

--- src/components/SendFriendshipRequest.tsx
+++ src/components/SendFriendshipRequest.tsx
@@ -5,29 +5,33 @@ import { IoMdPersonAdd as AddFriendIcon } from "react-icons/io";
 import { FaClock as ClockIcon } from "react-icons/fa";
 
 import { Spinner } from "./Spinner";
+import { useState } from "react";
+import { getMyId } from "@/utils/getMyId";
 
 interface SendFriendshipRequestProps {
   id: string;
-  isAccept: boolean;
+  isAccept: boolean | undefined;
 }
 
 export const SendFriendshipRequest = ({
   id,
   isAccept,
 }: SendFriendshipRequestProps) => {
-  const { isError, isPending, handleFriendshipRequest } =
+  const { isPending, wasClicked, handleFriendshipRequest } =
     useSendFriendshipRequest();
 
   return (
     <button
       className="ml-auto bg-blue-200 p-2"
-      onClick={() => handleFriendshipRequest(id, isAccept)}
+      onClick={() => handleFriendshipRequest(id, isAccept, getMyId)}
     >
       {isAccept === false && <ClockIcon />}
 
+      {wasClicked && !isPending && <ClockIcon />}
+
       {isPending && <Spinner wh={16} />}
 
-      {!isPending && isAccept !== false && <AddFriendIcon />}
+      {!isPending && isAccept !== false && !wasClicked && <AddFriendIcon />}
     </button>
   );
 };
