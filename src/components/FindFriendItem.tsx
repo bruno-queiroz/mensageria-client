9eb0a416a795fb293cbd3a34480d31e8cc5c75e2
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-08 07:58:20 -0300

    Remove button

--- src/components/FindFriendItem.tsx
+++ src/components/FindFriendItem.tsx
@@ -1,10 +1,4 @@
-"use client";
 import { UserWithFriendshipRequest } from "@/services/types";
-import { Spinner } from "./Spinner";
-import { useSendFriendshipRequest } from "@/hooks/useSendFriendshipRequest";
-
-import { IoMdPersonAdd as AddFriendIcon } from "react-icons/io";
-import { FaClock as ClockIcon } from "react-icons/fa";
 
 export const FindFriendItem = ({
   name,
@@ -12,9 +6,6 @@ export const FindFriendItem = ({
   id,
   isAccept,
 }: UserWithFriendshipRequest) => {
-  const { isError, isPending, handleFriendshipRequest } =
-    useSendFriendshipRequest();
-
   return (
     <div className="flex w-full gap-4 items-center bg-blue-100 p-3 rounded-lg">
       <div className="w-[70px] h-[70px]">
@@ -26,17 +17,6 @@ export const FindFriendItem = ({
       </div>
 
       <span className="font-semibold">{name}</span>
-
-      <button
-        className="ml-auto bg-blue-200 p-2"
-        onClick={() => handleFriendshipRequest(id, isAccept)}
-      >
-        {isAccept === false && <ClockIcon />}
-
-        {isPending && <Spinner wh={16} />}
-
-        {!isPending && isAccept !== false && <AddFriendIcon />}
-      </button>
     </div>
   );
 };
