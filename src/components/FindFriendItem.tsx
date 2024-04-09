1f13e4b97b879735d5c89622fd26f8a3db0e9771
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-09 08:00:20 -0300

    Render components conditionally

--- src/components/FindFriendItem.tsx
+++ src/components/FindFriendItem.tsx
@@ -1,13 +1,20 @@
 import { UserWithFriendshipRequest } from "@/services/types";
+import { getMyId } from "@/utils/getMyId";
 import { ReactNode } from "react";
 
-type FindFriendItemProps = UserWithFriendshipRequest & { children: ReactNode };
+type FindFriendItemProps = UserWithFriendshipRequest & {
+  SendFriendshipRequest: ReactNode;
+  FriendshipRequestController: ReactNode;
+};
 
 export const FindFriendItem = ({
   name,
   image,
-  children,
+  toUser,
+  SendFriendshipRequest,
+  FriendshipRequestController,
 }: FindFriendItemProps) => {
+  const myId = getMyId();
   return (
     <div className="flex w-full gap-4 items-center bg-blue-100 p-3 rounded-lg">
       <div className="w-[70px] h-[70px]">
@@ -19,7 +26,8 @@ export const FindFriendItem = ({
       </div>
 
       <span className="font-semibold">{name}</span>
-      {children}
+
+      {myId === toUser ? FriendshipRequestController : SendFriendshipRequest}
     </div>
   );
 };
