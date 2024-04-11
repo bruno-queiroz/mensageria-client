55f57243ed2cb0e85aa2d1414e3fbb413b0ac7e3
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-11 09:49:07 -0300

    Create Friends component

---
+++ src/components/Friends.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { FaSearch as SearchIcon } from "react-icons/fa";
+import { startSocket } from "@/ws";
+import { FormEvent } from "react";
+import { useFriends } from "@/hooks/useFriends";
+import { Friend } from "./Friend";
+
+export const Friends = () => {
+  const { friends } = useFriends();
+
+  const test = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    startSocket("http://localhost:3333", 3010);
+  };
+  return (
+    <div className="flex flex-col gap-2 p-2 bg-gray-400 w-full">
+      <form onSubmit={test}>
+        <input className="mb-4" type="text" />
+        <button>
+          <SearchIcon />
+        </button>
+      </form>
+      {friends?.data?.map((friend, i) => (
+        <Friend key={i} name={friend.name} />
+      ))}
+    </div>
+  );
+};
