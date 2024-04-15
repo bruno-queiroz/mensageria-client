2f339948341e9911cb51ed08b936e64a7ec477b6
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-15 08:27:57 -0300

    Create getMessage fetch function

---
+++ src/services/message/getMessage.ts
@@ -0,0 +1,22 @@
+import { getAuthCookie } from "@/utils/getAuthCookie";
+import { ServerResponse } from "../types";
+import { MessageUser, PrivateMessage } from "./types";
+
+export type GetMessage = { user: MessageUser; messages: PrivateMessage[] };
+
+export const getMessage = async (to: string | undefined) => {
+  const authCookie = getAuthCookie();
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/message?toUser=${to}`,
+    {
+      credentials: "include",
+      headers: {
+        Cookie: authCookie,
+        Origin: process.env.NEXT_PUBLIC_DEPLOY_URL!,
+      },
+    }
+  );
+  const data: ServerResponse<GetMessage> = await response.json();
+  return data;
+};
