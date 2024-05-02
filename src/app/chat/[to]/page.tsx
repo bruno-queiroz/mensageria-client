e47d2fca320494c067a8c6843e538df384edbab7
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-05-02 11:37:45 -0300

    Pass url params to Chat component

--- src/app/chat/[to]/page.tsx
+++ src/app/chat/[to]/page.tsx
@@ -1,11 +1,9 @@
 import { Chat } from "@/components/Chat";
-import { getMessage } from "@/services/message/getMessage";
 
-export default async function Page({ params }: { params: { to: string } }) {
-  const data = await getMessage(params.to);
+export default function Page({ params }: { params: { to: string } }) {
   return (
     <section>
-      <Chat {...data} />
+      <Chat to={params.to} />
     </section>
   );
 }
