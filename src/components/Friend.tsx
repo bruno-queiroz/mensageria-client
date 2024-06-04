895aa228089a865c92d9a2d25122281a47708171
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-06-04 08:17:50 -0300

    Add Link to chat

--- src/components/Friend.tsx
+++ src/components/Friend.tsx
@@ -1,17 +1,20 @@
-export function Friend({ name }: { name: string }) {
-  const changeCurrentChat = () => {};
+import { useCurrentChat } from "@/hooks/useCurrentChat";
+import { MessageUser } from "@/services/message/types";
+import Link from "next/link";
+
+export function Friend(user: MessageUser) {
   return (
-    <div
+    <Link
+      href={`/chat/${user.id}`}
       className="flex gap-4 p-4 rounded bg-blue-50 cursor-pointer hover:bg-blue-100"
-      onClick={changeCurrentChat}
     >
       <div className="w-[60px] h-[60px] bg-blue-300 rounded-full">
-        <img src="" alt="" />
+        <img src={user.image} alt="" className="rounded-full" />
       </div>
       <div className="flex flex-col">
-        <span>{name}</span>
+        <span>{user.name}</span>
         <span>say hello</span>
       </div>
-    </div>
+    </Link>
   );
 }
