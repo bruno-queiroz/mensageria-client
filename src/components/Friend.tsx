b4d5a190a5441e2b5eb8709ea7161ef2b502ee7e
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-06-05 11:25:39 -0300

    Show last_message and message_amount

--- src/components/Friend.tsx
+++ src/components/Friend.tsx
@@ -6,14 +6,26 @@ export function Friend(user: MessageUser) {
   return (
     <Link
       href={`/chat/${user.id}`}
-      className="flex gap-4 p-4 rounded bg-blue-50 cursor-pointer hover:bg-blue-100"
+      className="flex items-center gap-4 p-4 rounded bg-blue-50 cursor-pointer hover:bg-blue-100"
     >
       <div className="w-[60px] h-[60px] bg-blue-300 rounded-full">
-        <img src={user.image} alt="" className="rounded-full" />
+        <img src={user?.image} alt="" className="rounded-full" />
       </div>
       <div className="flex flex-col">
         <span>{user.name}</span>
-        <span>say hello</span>
+        {user?.last_message.length ? (
+          <span>{user.last_message}</span>
+        ) : (
+          <span>say hello</span>
+        )}
+      </div>
+
+      <div className="ml-auto">
+        {user?.message_amount.length && (
+          <div className="py-[2px] px-2 text-white bg-blue-400 rounded-full">
+            {user.message_amount}
+          </div>
+        )}
       </div>
     </Link>
   );
