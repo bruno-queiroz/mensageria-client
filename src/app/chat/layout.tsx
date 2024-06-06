1689b5c860601b98364312fc336f2341dfd34ad7
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-06-06 08:47:29 -0300

    Fix chat height and add overflow auto

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -3,14 +3,12 @@ import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <>
-      <section className="bg-gray-200">
-        <div className="bg-blue-300 w-11 h-full fixed"></div>
-        <div className="grid grid-cols-chat h-full ml-11 pl-2 gap-2">
-          <Friends />
-          {children}
-        </div>
-      </section>
-    </>
+    <section className="bg-gray-200 h-[calc(100%-78px)]">
+      <div className="bg-blue-300 w-11 h-full fixed"></div>
+      <div className="grid grid-cols-chat h-full ml-11 pl-2 gap-2">
+        <Friends />
+        <div className="overflow-auto">{children}</div>
+      </div>
+    </section>
   );
 }
