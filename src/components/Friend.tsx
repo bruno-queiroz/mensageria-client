3cd3b1c6ad732a1c2c32ea60d077ce394b98420b
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-06-07 09:38:26 -0300

    Add styling to text

--- src/components/Friend.tsx
+++ src/components/Friend.tsx
@@ -12,16 +12,18 @@ export function Friend(user: MessageUser) {
         <img src={user?.image} alt="" className="rounded-full" />
       </div>
       <div className="flex flex-col">
-        <span>{user.name}</span>
-        {user?.last_message.length ? (
-          <span>{user.last_message}</span>
-        ) : (
-          <span>say hello</span>
-        )}
+        <span className="font-semibold">{user.name}</span>
+        <div className="text-gray-600">
+          {user?.last_message?.length ? (
+            <span>{user.last_message}</span>
+          ) : (
+            <span>say hello</span>
+          )}
+        </div>
       </div>
 
       <div className="ml-auto">
-        {user?.message_amount.length && (
+        {user?.message_amount > 0 && (
           <div className="py-[2px] px-2 text-white bg-blue-400 rounded-full">
             {user.message_amount}
           </div>
