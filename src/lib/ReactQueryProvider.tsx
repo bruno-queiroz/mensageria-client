0a624b0a2e68286ab26eca04c90e6f8aa86b6a0c
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-06-12 16:12:51 -0300

    Set refetchOnWindowFocus to false

--- src/lib/ReactQueryProvider.tsx
+++ src/lib/ReactQueryProvider.tsx
@@ -2,7 +2,9 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React, { ReactNode } from "react";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
 
 export function ReactQueryProvider({ children }: { children: ReactNode }) {
   return (
