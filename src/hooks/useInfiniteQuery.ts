426d7c0e2bc5f6ac5d2bf5c9af5c2e49a4c33b82
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-07-12 11:40:40 -0300

    Create useInfiniteQuery hook

---
+++ src/hooks/useInfiniteQuery.ts
@@ -0,0 +1,30 @@
+import { GetMessage, getMessage } from "@/services/message/getMessage";
+import { ServerResponse } from "@/services/types";
+import { useEffect, useState } from "react";
+
+export const useInfiniteQuery = (to: string) => {
+  const [data, setDate] = useState<ServerResponse<GetMessage>[]>([]);
+
+  const fetchNextPage = async () => {
+    const newData = await getMessage({
+      date: data[data.length - 1].data?.messages[0].sentAt,
+      to,
+    });
+    setDate([...data, newData]);
+  };
+
+  useEffect(() => {
+    (async () => {
+      const newData = await getMessage({
+        date: new Date().toISOString(),
+        to: to,
+      });
+      setDate([...data, newData]);
+    })();
+  }, []);
+
+  return {
+    data,
+    fetchNextPage,
+  };
+};
