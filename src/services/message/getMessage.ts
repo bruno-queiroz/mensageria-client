99c39251e4a0d30a514cbfb4bd5a20c0d4ca32f0
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-07-15 11:23:21 -0300

    Add mode param to fetch function

--- src/services/message/getMessage.ts
+++ src/services/message/getMessage.ts
@@ -10,7 +10,7 @@ export type GetMessage = {
 
 export const getMessage = async (pageParam: any) => {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/message?toUser=${pageParam.to}&date=${pageParam.date}`,
+    `${process.env.NEXT_PUBLIC_API_URL}/api/message?toUser=${pageParam.to}&date=${pageParam.date}&mode=${pageParam.mode}`,
     {
       method: "PATCH",
       credentials: "include",
