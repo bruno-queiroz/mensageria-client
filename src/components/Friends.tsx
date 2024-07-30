1bf061b9de7b47485234f9ed650560a25888c14a
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-07-30 09:03:41 -0300

    Add FilterFriends component

--- src/components/Friends.tsx
+++ src/components/Friends.tsx
@@ -1,21 +1,16 @@
 "use client";
-import { FaSearch as SearchIcon } from "react-icons/fa";
 import { startSocket } from "@/ws";
 import { FormEvent } from "react";
 import { useFriends } from "@/hooks/useFriends";
 import { Friend } from "./Friend";
+import { FilterFriends } from "./FilterFriends";
 
 export const Friends = () => {
   const { friends } = useFriends();
 
   return (
-    <div className="flex flex-col gap-2 p-2 bg-gray-400 w-full overflow-auto">
-      <form>
-        <input className="mb-4" type="text" />
-        <button>
-          <SearchIcon />
-        </button>
-      </form>
+    <div className="flex flex-col gap-4 p-2 bg-gray-400 w-full overflow-auto">
+      <FilterFriends />
 
       <div className="flex flex-col gap-2">
         {friends?.data?.map((friend, i) => (
