73e4d2143f542539b534d2dbe86802239f29f413
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-08-02 15:22:58 -0300

    Get isSuccess and isRefetching from useQuery

--- src/hooks/useFriends.ts
+++ src/hooks/useFriends.ts
@@ -2,12 +2,18 @@ import { getFriends } from "@/services/friend/getFriends";
 import { useQuery } from "@tanstack/react-query";
 
 export const useFriends = () => {
-  const { data: friends, isPending } = useQuery({
+  const {
+    data: friends,
+    isSuccess,
+    isRefetching,
+  } = useQuery({
     queryKey: ["getFriend"],
     queryFn: getFriends,
   });
 
   return {
     friends,
+    isRefetching,
+    isSuccess,
   };
 };
