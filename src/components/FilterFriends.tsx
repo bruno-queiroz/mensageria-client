ecbb6467b40dab5f84c6ec7202795868d03c87c7
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-08-09 10:58:10 -0300

    Create FilterFriendsProps interface

--- src/components/FilterFriends.tsx
+++ src/components/FilterFriends.tsx
@@ -1,9 +1,19 @@
+import { FormEvent, useRef } from "react";
 import { FaSearch as SearchIcon } from "react-icons/fa";
 
-export const FilterFriends = () => {
+interface FilterFriendsProps {
+  filter: (e: FormEvent<HTMLFormElement>, name: string) => void;
+}
+
+export const FilterFriends = ({ filter }: FilterFriendsProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="flex">
-      <input className="w-full p-1" type="text" />
+    <form
+      className="flex"
+      onSubmit={(e) => filter(e, inputRef.current?.value || "")}
+    >
+      <input className="w-full p-1" type="text" ref={inputRef} />
       <button className="bg-blue-300 py-1 px-4 text-white">
         <SearchIcon />
       </button>
